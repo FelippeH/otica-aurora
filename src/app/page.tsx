e23fb34d5a2e52e65426cfb86c085f023fef5489
FelippeH <felipeminasgeraisbrasil@gmail.com>
2025-08-17 05:23:36 -0300

    feat: atualiza o layout do componente Home para melhorar a exibição do produto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h2>...</h2>
+    <div className="max-w-7xl mx-auto pt-8 px-10 md:px-0">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 md:gap-6">
+        <div className="bg-primary rounded-lg shadow-lg overflow-hidden">
+          Produto 1
+        </div>
+        <div className="bg-primary rounded-lg shadow-lg overflow-hidden">
+          Produto 2
+        </div>
+        <div className="bg-primary rounded-lg shadow-lg overflow-hidden">
+          Produto 3
+        </div>
+        <div className="bg-primary rounded-lg shadow-lg overflow-hidden">
+          Produto 4
+        </div>
+        <div className="bg-primary rounded-lg shadow-lg overflow-hidden">
+          Produto 5
+        </div>
+        <div className="bg-primary rounded-lg shadow-lg overflow-hidden">
+          Produto 6
+        </div>
+        <div className="bg-primary rounded-lg shadow-lg overflow-hidden">
+          Produto 7
+        </div>
+        <div className="bg-primary rounded-lg shadow-lg overflow-hidden">
+          Produto 8
+        </div>
+      </div>
     </div>
   );
 }
