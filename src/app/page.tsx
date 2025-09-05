69d44781cfdb5a2e3be77bc17ef09e557e454652
FelippeH <felipeminasgeraisbrasil@gmail.com>
2025-09-05 19:03:20 -0300

    feat: refatoração da busca de produtos para usar a API Stripe e atualizar a lógica de exibição do produto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,32 @@
 import ProductCarousel from "@/components/layout/ProductCarousel";
 import Cover from "@/components/layout/Cover";
 import Info from "@/components/layout/Info";
+import { Product } from "@/types/ProductType";
+import { Stripe } from "stripe";
 
 // Função para buscar produtos da API
-async function getProducts() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`, {
-    cache: "no-store",
+async function getProducts(): Promise<Product[]> {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+    apiVersion: "2025-08-27.basil",
   });
 
-  if (!res.ok) throw new Error("Falha ao buscar produtos");
+  const products = await stripe.products.list({
+    expand: ["data.default_price"],
+  });
+  const formattedProducts: Product[] = products.data.map((product) => {
+    const price = product.default_price as Stripe.Price;
 
-  return res.json();
+    return {
+      id: product.id,
+      name: product.name,
+      description: product.description || "",
+      price: (price.unit_amount || 0) / 100,
+      image: product.images.length > 0 ? product.images : ["/placeholder.png"],
+      currency: price.currency,
+      category: product.metadata.category || "uncategorized",
+    };
+  });
+  return formattedProducts;
 }
 
 export default async function HomePage() {
@@ -30,7 +46,7 @@ export default async function HomePage() {
         <p className="md:text-[1.4rem] md:mt-8 mt-8 text-[1.1rem] font-bold mb-6 text-center">
           AS MELHORES OPÇÕES EM ÓCULOS SOL
         </p>
-        <ProductCarousel products={products} category="sunglass" />
+        <ProductCarousel products={products} category="sunglasses" />
       </div>
     </main>
   );
--- src/components/layout/ProductCard.tsx
+++ src/components/layout/ProductCard.tsx
@@ -1,33 +1,26 @@
 "use client";
 
 import { Product } from "@/types/ProductType";
-import { useState } from "react";
 import Image from "next/image";
+import { formatPrice } from "@/lib/utils";
 
 export default function ProductCard({ product }: { product: Product }) {
-  const [hover, setHover] = useState(false);
-
   return (
-    <div
-      className="bg-white border-[1px] border-[#ededed] rounded-[10px] p-4 hover:border-gray-400 transition w-full max-w-xs mx-auto cursor-pointer"
-      onMouseEnter={() => setHover(true)}
-      onMouseLeave={() => setHover(false)}
-    >
+    <div className="border border-gray-200 rounded-lg p-4 hover:shadow-lg transition-shadow duration-300 bg-white">
       {/* Container da imagem fluido */}
       <div className="relative w-full">
         <Image
-          src={hover ? product.images[1] : product.images[0]}
-          alt={product.title}
+          src={product.image[0]}
+          alt={product.name}
           width={500}
           height={500}
           className="w-full h-auto object-contain rounded-lg transition-all duration-300"
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
         />
       </div>
 
       <div className="flex flex-col text-center">
-        <h2 className="text-xl font-bold mt-2">{product.title}</h2>
-        <p className="text-gray-600 text-xl">R${product.price}</p>
+        <h2 className="text-xl font-bold mt-2">{product.name}</h2>
+        <p className="text-gray-600 text-xl">{formatPrice(product.price)}</p>
       </div>
     </div>
   );
