31d96503539d07195bfccff2f03ea8d65fac9fe8
FelippeH <felipeminasgeraisbrasil@gmail.com>
2025-09-07 11:47:55 -0300

    feat: remove API route de products

--- src/app/api/products/route.ts
+++
@@ -1,8 +0,0 @@
-import products from "@/data/products.json";
-import { NextResponse } from "next/server";
-import { Product } from "@/types/ProductType";
-
-// Função para manipular a requisição GET
-export async function GET() {
-  return NextResponse.json(products as Product[]);
-}
